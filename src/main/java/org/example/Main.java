227fc0886f293b5593dc2d630e17c7cf437c472d
csahinn <csahinn@outlook.com.tr>
2024-10-19 22:47:10 +0300

    minor bug fixes :d

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.net.URL;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.println("Welcome to my Case Study for Deloitte!");
+        System.out.println("Developed by Cagatay Sahin");
+        }
+}
